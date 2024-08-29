83a04f9dca614d02864779d0c16495a1da35b0c7
Allexandrres <alexandredasilveira13@gmail.com>
2024-08-29 12:16:37 -0300

    Add files via upload

---
+++ atividades/script.js
@@ -0,0 +1,52 @@
+//atividade 1
+
+// const numeros = [50, 45, 67, 89, 10, 5];
+
+// numeros.forEach(numero => {
+//     const dobro = numero * 2;
+//     console.log(`O dobro de ${numero} é ${dobro}`);
+// });
+
+//atividade 2
+
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// const numerosPares = numeros.filter(numero => numero % 2 === 0);
+
+// console.log('Números pares:');
+// numerosPares.forEach(numero => {
+//     console.log(numero);
+// });
+
+// atividade 3
+
+// const numeros = [1, 2, 3, 4, 5, 6];
+
+// const soma = numeros.reduce((total, numero) => total + numero, 0);
+
+// console.log('A soma de todos os números é: ' + soma);
+
+//atividade 4
+
+// const numeros = [2, 4, 6, 8, 10];
+
+// numeros.forEach(numero => {
+//     const quadrado = numero ** 2;
+//     console.log(`${numero} elevado ao quadrado é: ${quadrado}`);
+// });
+
+//atividade 5
+
+// const listaDeStrings = ['banana', 'maçã', 'laranja', 'morango'];
+
+// const listaMaiuscula = listaDeStrings.map(string => string.toUpperCase());
+
+// console.log(listaMaiuscula);
+
+//atividade 6
+
+// const listaDeStrings = ['cachorro', 'gato', 'elefante', 'pássaro', 'tartaruga'];
+
+// const palavrasComMaisDe5Caracteres = listaDeStrings.filter(string => string.length > 5);
+
+// console.log(palavrasComMaisDe5Caracteres);
